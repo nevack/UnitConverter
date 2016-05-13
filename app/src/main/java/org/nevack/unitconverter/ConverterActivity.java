83233f795db7f53d48542b7d287b60e855b05169
Dmitry (nevack) Nevedomsky <dimchik01050@gmail.com>
2016-05-14 00:13:43 +0300

    App version bump Removed fragments Added swap button interaction with snackbar

--- app/src/main/java/org/nevack/unitconverter/ConverterActivity.java
+++ app/src/main/java/org/nevack/unitconverter/ConverterActivity.java
@@ -1,9 +1,9 @@
 package org.nevack.unitconverter;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -13,20 +13,42 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import org.nevack.unitconverter.fragments.ConverterFragment;
 import org.nevack.unitconverter.model.EUnitCategory;
+import org.nevack.unitconverter.model.converter.Converter;
 
 public class ConverterActivity extends AppCompatActivity {
+
+    private static final String CATEGORY_ID = "category_id";
+    private static final String COPY_RESULT = "converter_result";
 
     private Toolbar mToolbar;
     private FloatingActionButton mFab;
     private NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
     private CoordinatorLayout mCoordinatorLayout;
+
+    private Spinner mSourceSpinner;
+    private EditText mSourceEditText;
+    private TextView mSourceTextView;
+    private EditText mResultEditText;
+    private TextView mResultTextView;
+    private Spinner mResultSpinner;
+    private Button mSwapImageButton;
+
+    private Converter mConverter;
+    private AsyncTask mTask;
+    private SpinnerListener mListener = new SpinnerListener();
 
     private int converter_id = 0;
 
@@ -39,12 +61,6 @@ public class ConverterActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        converter_id = getIntent().getIntExtra("ConverterID", 0);
-        final FragmentManager manager = getFragmentManager();
-        final ConverterFragment fragment = ConverterFragment
-                .newInstance(EUnitCategory.values()[converter_id]);
-        manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
@@ -61,14 +77,9 @@ public class ConverterActivity extends AppCompatActivity {
                 menuItem.setChecked(true);
                 converter_id = menuItem.getOrder();
 
-                ConverterFragment converterFragment = ConverterFragment
-                        .newInstance(EUnitCategory.values()[converter_id]);
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_container, converterFragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
                 mDrawerLayout.closeDrawers();
+
+                setConverter();
                 return true;
             }
         });
@@ -79,21 +90,79 @@ public class ConverterActivity extends AppCompatActivity {
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                copyResultToClipboard(fragment.getResult(false));
-                Snackbar.make(mCoordinatorLayout, R.string.copy_result_toast, Snackbar.LENGTH_SHORT)
-                        .show();
+                copyResultToClipboard(false);
             }
         });
 
         mFab.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                copyResultToClipboard(fragment.getResult(true));
-                Snackbar.make(mCoordinatorLayout, R.string.copy_result_toast, Snackbar.LENGTH_SHORT)
-                        .show();
+                copyResultToClipboard(true);
                 return true;
             }
         });
+
+        mSourceSpinner = (Spinner) findViewById(R.id.sourcespinner);
+        mSourceSpinner.setOnItemSelectedListener(new SpinnerListener());
+
+        mResultSpinner = (Spinner) findViewById(R.id.resultspinner);
+        mResultSpinner.setOnItemSelectedListener(new SpinnerListener());
+
+        mSourceEditText = (EditText) findViewById(R.id.sourcevalue);
+        mSourceEditText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mSourceEditText.getText().toString().equals(".")) {
+                    mSourceEditText.setText("0.");
+                    mSourceEditText.setSelection(mSourceEditText.getText().toString().length());
+                }
+                if (!mSourceEditText.getText().toString().equals("-")) convert();
+            }
+        });
+        mResultEditText = (EditText) findViewById(R.id.resultvalue);
+
+        mSourceTextView = (TextView) findViewById(R.id.sourcevaluesymbol);
+        mResultTextView = (TextView) findViewById(R.id.resultvaluesymbol);
+
+        mSwapImageButton = (Button) findViewById(R.id.swapbutton);
+        mSwapImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = mSourceEditText.getText().toString();
+                mSourceEditText.setText(mResultEditText.getText().toString());
+                mResultEditText.setText(temp);
+                int position = mSourceSpinner.getSelectedItemPosition();
+                mSourceSpinner.setSelection(mResultSpinner.getSelectedItemPosition());
+                mResultSpinner.setSelection(position);
+                convert();
+            }
+        });
+
+        mSourceTextView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { mSourceSpinner.performClick(); }
+        });
+
+        mResultTextView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { mResultSpinner.performClick(); }
+        });
+
+        converter_id = getIntent().getIntExtra("ConverterID", 0);
+        setConverter();
+    }
+
+    private void setConverter() {
+        new InitDataAsync().execute();
+    }
+
+    private void convert() {
+        if (mSourceEditText.getText().toString().equals("")) mResultEditText.setText("");
+        else mResultEditText.setText(mConverter.convert(mSourceEditText.getText().toString(),
+                mSourceSpinner.getSelectedItemPosition(),
+                mResultSpinner.getSelectedItemPosition()));
     }
 
     @Override
@@ -116,14 +185,85 @@ public class ConverterActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(CATEGORY_ID, converter_id);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        converter_id = savedInstanceState.getInt(CATEGORY_ID);
+        setConverter();
+    }
+
+    @Override
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.leave_in_anim, R.anim.leave_out_anim);
     }
 
-    private void copyResultToClipboard(ClipData clipData) {
+    private void copyResultToClipboard(boolean withUnitSymbol) {
+        String textToCopy = "";
+        if (!mResultEditText.getText().toString().equals("")) {
+            textToCopy = mResultEditText.getText().toString();
+        }
+        if (withUnitSymbol)
+        {
+            textToCopy += mResultTextView.getText().toString();
+        }
         ClipboardManager clipboard = (ClipboardManager)
                 getSystemService(CLIPBOARD_SERVICE);
-        clipboard.setPrimaryClip(clipData);
+        clipboard.setPrimaryClip(ClipData.newPlainText(COPY_RESULT, textToCopy));
+        Snackbar.make(mCoordinatorLayout, R.string.copy_result_toast, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private class SpinnerListener implements Spinner.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            mSourceTextView.setText(mConverter.getUnitSymbol(mSourceSpinner.getSelectedItemPosition()));
+            mResultTextView.setText(mConverter.getUnitSymbol(mResultSpinner.getSelectedItemPosition()));
+            convert();
+        }
+
+        @Override public void onNothingSelected(AdapterView<?> parent) { convert(); }
+    }
+
+    private class InitDataAsync extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog mDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mSourceEditText.setText("");
+            mResultEditText.setText("");
+
+            mDialog = new ProgressDialog(ConverterActivity.this);
+            mDialog.setMessage(getString(R.string.loading_data));
+            mDialog.setCancelable(false);
+            mDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            setTitle(mConverter.getTitle());
+
+            mSourceSpinner.setAdapter(mConverter.getAdapter());
+            mResultSpinner.setAdapter(mConverter.getAdapter());
+            mSourceTextView.setText(mConverter.getUnitSymbol(0));
+            mResultTextView.setText(mConverter.getUnitSymbol(0));
+
+            if (mDialog != null && mDialog.isShowing()) mDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            mConverter = EUnitCategory.values()[converter_id].getConverter(ConverterActivity.this);
+
+            return null;
+        }
     }
 }

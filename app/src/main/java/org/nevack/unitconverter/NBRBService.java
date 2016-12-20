3ed6bc525de816e2a0b988050f3525b9b8686baa
Dmitry Nevedomsky <dimchik01050@gmail.com>
2016-12-20 20:54:49 +0300

    Preparing to migrate to new NBRB api

---
+++ app/src/main/java/org/nevack/unitconverter/NBRBService.java
@@ -0,0 +1,28 @@
+package org.nevack.unitconverter;
+
+import org.nevack.unitconverter.model.Currency;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface NBRBService {
+    @GET("/Currencies")
+    Call<List<Currency>> listAllCurrencies();
+
+    @GET("/Currencies/{id}")
+    Call<List<Currency>> getCurrencyById(@Path("id") String currencyId);
+
+    @GET("/Rates?Periodicity=0")
+    Call<List<Currency>> getAllRatesForToday();
+
+    @GET("/Rates?Periodicity=0")
+    Call<List<Currency>> getAllRatesForDate(@Query("onDate") String date);
+
+    @GET("/Rates?Periodicity=1")
+    Call<List<Currency>> getAllRatesForThisMonth();
+}
+

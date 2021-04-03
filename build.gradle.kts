e60dcc3cfcf75f31ec69da45799cd7e4e3ab013f
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-04-04 01:23:03 +0300

    Migrate to Kts, update deps and Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+import com.diffplug.gradle.spotless.SpotlessPlugin
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-alpha12")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0-M2")
+
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
+    }
+}
+
+plugins {
+    id("com.diffplug.spotless") version "5.11.1"
+    id("com.github.ben-manes.versions") version "0.38.0"
+}
+
+subprojects {
+    apply<SpotlessPlugin>()
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+
+            ktlint("0.40.0")
+        }
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            // Treat all Kotlin warnings as errors
+            allWarningsAsErrors = true
+
+            // Set JVM target to 1.8
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+}

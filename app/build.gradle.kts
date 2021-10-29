0f5efe2fcd0cc5922030a62ea8cb24879848e622
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-10-29 23:36:35 +0300

    Use correct Kotlin Android Plugin declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("dagger.hilt.android.plugin")
 }

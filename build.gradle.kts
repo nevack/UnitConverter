2b905f5cbd89ab207a4d952c3911641c3b4e604b
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-11-10 00:43:07 +0300

    Fix build after Spotless 6.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,10 @@
 plugins {
-    id("com.diffplug.spotless") version "6.0.0"
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("com.github.spotbugs") version "4.7.9"
+    id("com.diffplug.spotless") version "6.0.0" apply false
     id("com.android.application") version "7.2.0-alpha04" apply false
     id("org.jetbrains.kotlin.android") version "1.6.0-RC2" apply false
     id("org.jetbrains.kotlin.kapt") version "1.6.0-RC2" apply false
     id("dagger.hilt.android.plugin") version "2.40" apply false
-}
-
-spotless {
-    kotlin {
-        target("**/*.kt")
-        targetExclude("$buildDir/**/*.kt")
-        targetExclude("bin/**/*.kt")
-
-        ktlint("0.43.0")
-    }
 }
 
 tasks.wrapper {

324288cf32d1019db2b11f5f9e7dc76a2a330673
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-11-10 00:16:01 +0300

    Roll Android Gradle Plugin 7.2.0-alpha04

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.diffplug.spotless") version "5.17.1"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("com.github.spotbugs") version "4.7.9"
-    id("com.android.application") version "7.2.0-alpha03" apply false
+    id("com.android.application") version "7.2.0-alpha04" apply false
     id("org.jetbrains.kotlin.android") version "1.6.0-RC2" apply false
     id("org.jetbrains.kotlin.kapt") version "1.6.0-RC2" apply false
     id("dagger.hilt.android.plugin") version "2.40" apply false

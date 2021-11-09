19ff33aab0be574f9813cdf6e630aecc6f4b11a4
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-11-10 00:52:53 +0300

    Let Kotlin Gradle Plugin add stdlib dep.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     // AndroidX
     implementation("androidx.core:core-ktx:1.7.0")

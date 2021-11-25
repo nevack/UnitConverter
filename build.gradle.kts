207274ccd537146821338ec980f4ab4b3da23a66
Dzmitry Neviadomski <nevack.d@gmail.com>
2021-11-25 05:42:52 +0300

    Bump Hilt and AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.39.0"
     id("com.diffplug.spotless") version "6.0.0" apply false
-    id("com.android.application") version "7.2.0-alpha04" apply false
+    id("com.android.application") version "7.2.0-alpha05" apply false
     id("org.jetbrains.kotlin.android") version "1.6.0" apply false
     id("org.jetbrains.kotlin.kapt") version "1.6.0" apply false
-    id("dagger.hilt.android.plugin") version "2.40" apply false
+    id("dagger.hilt.android.plugin") version "2.40.2" apply false
 }
 
 tasks.wrapper {

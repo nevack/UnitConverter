7592e3e09ce8d52927405bf0ed9a6119130a1ae5
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-01-29 02:35:47 +0300

    Rollback everything to stable.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,12 @@ dependencies {
     // Kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     // AndroidX
-    implementation("androidx.core:core-ktx:1.8.0-alpha03")
-    implementation("androidx.fragment:fragment-ktx:1.5.0-alpha01")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.5.0-alpha01")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.0-alpha01")
-    implementation("androidx.recyclerview:recyclerview:1.3.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
     // Material
     implementation("com.google.android.material:material:1.6.0-alpha02")
     // Room

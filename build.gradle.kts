20c6debbf9f0fc59bc26c187a681b83a4ce13d6e
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-04-23 17:41:30 +0300

    Update rejectVersionIf condition.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,18 +13,34 @@ tasks.wrapper {
     gradleVersion = "7.4.2"
 }
 
-fun isNotStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
+enum class AndroidStability {
+    ALPHA, BETA, RC, STABLE
+}
+
+fun getAndroidStability(version: String): AndroidStability = when {
+    version.contains("alpha") -> AndroidStability.ALPHA
+    version.contains("beta") -> AndroidStability.BETA
+    version.contains("rc") -> AndroidStability.RC
+    else -> AndroidStability.STABLE
+}
+
+fun isAndroidDep(group: String): Boolean = when {
+    group.startsWith("androidx.") -> true
+    group.startsWith("com.google.android.") -> true
+    group.startsWith("com.android.") -> true
+    else -> false
 }
 
 tasks.dependencyUpdates {
     checkForGradleUpdate = true
     gradleReleaseChannel = "current"
     rejectVersionIf {
-        if (candidate.module == "org.jacoco.ant") true
-        else isNotStable(candidate.version) && !isNotStable(currentVersion)
+        when {
+            candidate.module == "org.jacoco.ant" -> true
+            isAndroidDep(candidate.group) -> {
+                getAndroidStability(candidate.version) < getAndroidStability(currentVersion)
+            }
+            else -> false
+        }
     }
 }

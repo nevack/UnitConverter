e3d34d8959a4f7e550579e9335a2e28bb2df21fc
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-04-27 08:13:27 +0300

    Remove Sonatype Snapshots repo.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,10 +21,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-            mavenContent { snapshotsOnly() }
-        }
     }
 }
 

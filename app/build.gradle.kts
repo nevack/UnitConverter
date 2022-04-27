1ba04e0380c01ae40834b1da0c4f52bf9b843233
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-04-27 08:06:39 +0300

    Move Plugins versions to settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
-    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
     id("com.diffplug.spotless")
     id("org.gradle.android.cache-fix")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,16 @@ pluginManagement {
         google()
         mavenCentral()
     }
+
+    plugins {
+        id("com.github.ben-manes.versions") version "0.42.0"
+        id("com.diffplug.spotless") version "6.5.0"
+        id("com.android.application") version "7.1.3"
+        id("org.jetbrains.kotlin.android") version "1.6.21"
+        id("org.jetbrains.kotlin.kapt") version "1.6.21"
+        id("com.google.dagger.hilt.android") version "2.41"
+        id("org.gradle.android.cache-fix") version "2.5.1"
+    }
 }
 
 dependencyResolutionManagement {

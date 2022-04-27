dc26e8ba2a46912314ad6b8adf64fb39d5904e06
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-04-27 08:07:10 +0300

    Roll org.gradle.android.cache-fix 2.5.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.android") version "1.6.21"
         id("org.jetbrains.kotlin.kapt") version "1.6.21"
         id("com.google.dagger.hilt.android") version "2.41"
-        id("org.gradle.android.cache-fix") version "2.5.1"
+        id("org.gradle.android.cache-fix") version "2.5.2"
     }
 }
 

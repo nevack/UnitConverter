9a74db97584e4ab81e6425fe78755d40d4891d78
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-06-08 01:39:13 +0300

    Roll androidx.core:core-ktx:1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     // Kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2")
     // AndroidX
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.4.1")

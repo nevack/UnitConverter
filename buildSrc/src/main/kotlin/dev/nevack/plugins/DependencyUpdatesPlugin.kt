f9bc8c6f79d00eab15cb337fae253e3cdf6197f6
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-07-17 04:14:04 +0300

    Add ignoreConstraints property.

--- buildSrc/src/main/kotlin/dev/nevack/plugins/DependencyUpdatesPlugin.kt
+++ buildSrc/src/main/kotlin/dev/nevack/plugins/DependencyUpdatesPlugin.kt
@@ -4,16 +4,21 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.provideDelegate
 
 class DependencyUpdatesPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("com.github.ben-manes.versions")
+
+        val ignoreConstraints: String? by target
+        val ignoreConstraintsBoolean = ignoreConstraints?.toBoolean() ?: false
 
         target.tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
             checkForGradleUpdate = true
             gradleReleaseChannel = "current"
             rejectVersionIf {
                 when {
+                    ignoreConstraintsBoolean -> false
                     candidate.module == "org.jacoco.ant" -> candidate.version != currentVersion
                     isAndroidDep(candidate.group) -> {
                         getAndroidStability(candidate.version) < getAndroidStability(currentVersion)

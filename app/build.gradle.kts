1111830023481a93cea1229447bf6b9ce5c6a52d
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-08-02 02:10:36 +0300

    Roll androidx.lifecycle 2.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.5.1")
     implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     // Material
     implementation("com.google.android.material:material:1.6.1")

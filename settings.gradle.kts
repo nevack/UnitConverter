3061d4e9bc3daaf9c0910a8df5d955389b7ef575
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-09-16 04:31:08 +0300

    Roll com.google.gms.google-services 4.3.14

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.kapt") version "1.7.20-RC"
         id("com.google.dagger.hilt.android") version "2.43.2"
         id("org.gradle.android.cache-fix") version "2.5.6"
-        id("com.google.gms.google-services") version "4.3.13"
+        id("com.google.gms.google-services") version "4.3.14"
         id("com.google.firebase.crashlytics") version "2.9.1"
     }
 }

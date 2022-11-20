681b8ea66673d82c9c360b320f55bd64d446b97e
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-11-21 00:36:30 +0300

    Bump firebase-bom 31.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.44.2")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:31.0.3"))
+    implementation(platform("com.google.firebase:firebase-bom:31.1.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 

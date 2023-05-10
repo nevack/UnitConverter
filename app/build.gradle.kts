8f0bc4938250329b90b90c51410830e63ca2e283
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-05-10 23:47:59 +0300

    Roll firebase-bom 32.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.46")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:31.5.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 

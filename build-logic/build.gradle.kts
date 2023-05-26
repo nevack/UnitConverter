d9215070053a00662cfd47ae0cfb4d1a4886cf71
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-05-26 05:02:11 +0300

    Roll AGP 8.0.2

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle-api:8.0.1")
+    implementation("com.android.tools.build:gradle-api:8.0.2")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
 }
 

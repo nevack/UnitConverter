0d76b2c41a4b65d008c2690a473b2c95c23d0fba
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-07-27 17:23:38 +0300

    Roll androidx.recyclerview 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
     // Material
     implementation("com.google.android.material:material:1.9.0")
     // Room

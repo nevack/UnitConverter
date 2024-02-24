986e593ca8b88d25014ad30f27dac479c473b122
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-02-24 04:53:54 +0300

    Remove no longer needed android.compileOptions as of AGP 8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,8 +82,3 @@ dependencies {
 spotless.kotlin { ktlint() }
 
 kotlin.jvmToolchain(17)
-
-android.compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}

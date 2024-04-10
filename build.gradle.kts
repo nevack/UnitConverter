488703416ea7284229d52499279f1a0108d64493
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-04-11 02:49:10 +0300

    List all plugins in root project to fix class loader violations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 plugins {
+    id("com.github.ben-manes.versions") apply false
+    id("com.diffplug.spotless") apply false
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.google.devtools.ksp") apply false
+    id("com.google.dagger.hilt.android") apply false
+    id("org.gradle.android.cache-fix") apply false
+    id("com.google.gms.google-services") apply false
+    id("com.google.firebase.crashlytics") apply false
     id("dev.nevack.plugins.dependency-updates")
 }
 

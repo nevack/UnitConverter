e47186414df3c6e900434435eebddc74a4398029
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-05-03 11:32:08 +0300

    Roll androidx.appcompat 1.7.0-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.8.0-alpha02")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0-beta01")
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")

a4e74d271c7c17a0505453b2a5e712cfdeaf975f
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-05-16 01:52:32 +0300

    Roll AGP 8.5.0-beta02

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
         val kspSubVersion = "1.0.20"
         id("com.github.ben-manes.versions") version "0.51.0"
         id("com.diffplug.spotless") version "6.25.0"
-        id("com.android.application") version "8.5.0-beta01"
+        id("com.android.application") version "8.5.0-beta02"
         id("org.jetbrains.kotlin.android") version kotlinVersion
         id("com.google.devtools.ksp") version "$kotlinVersion-$kspSubVersion"
         id("com.google.dagger.hilt.android") version "2.51.1"

6a5a32372c24a4b78e7703d52115f2eb103eecd6
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-12-11 10:07:54 +0300

    Fixup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation("dev.chrisbanes.insetter:insetter:0.6.1")
     // Dagger + Hilt
     implementation("com.google.dagger:hilt-android:2.53.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.53.1.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.53.1")
 
     // Test
     testImplementation("junit:junit:4.13.2")

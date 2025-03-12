ecca592b921b2fca7f9ae65365a4588d84c07280
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-03-12 03:17:51 +0300

    Roll androidx.room 2.7.0-rc01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     // Room
-    implementation("androidx.room:room-common:2.7.0-beta01")
-    implementation("androidx.room:room-runtime:2.7.0-beta01")
-    ksp("androidx.room:room-compiler:2.7.0-beta01")
+    implementation("androidx.room:room-common:2.7.0-rc01")
+    implementation("androidx.room:room-runtime:2.7.0-rc01")
+    ksp("androidx.room:room-compiler:2.7.0-rc01")
     implementation("androidx.sqlite:sqlite:2.4.0")
     // Okio
     implementation("com.squareup.okio:okio:3.10.2")

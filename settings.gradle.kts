3dc4c311340dee9acdc6b905768180de5f4f84ef
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-03-21 02:04:34 +0300

    Roll Kotlin 2.1.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     }
 
     plugins {
-        val kotlinVersion = "2.1.20-RC3"
+        val kotlinVersion = "2.1.20"
         val kspSubVersion = "1.0.31"
         id("com.github.ben-manes.versions") version "0.52.0"
         id("com.diffplug.spotless") version "7.0.2"

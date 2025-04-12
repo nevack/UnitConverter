344cc21bcfcb1c289d957a2c920b7a10a7c49529
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-04-12 04:17:04 +0300

    Roll androidx.core 1.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-android")
     // AndroidX
     implementation("androidx.annotation:annotation:1.9.1")
-    implementation("androidx.core:core-ktx:1.16.0-rc01")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.appcompat:appcompat:1.7.0")

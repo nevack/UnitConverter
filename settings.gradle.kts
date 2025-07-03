38733924b600c806aad88f818445aaa07772bd82
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-07-04 00:47:52 +0300

    Roll com.autonomousapps.dependency-analysis 2.19.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
         id("com.google.devtools.ksp") version "$kotlinVersion-$kspSubVersion"
         id("com.google.dagger.hilt.android") version "2.56.2"
         id("org.gradle.android.cache-fix") version "3.0.1"
-        id("com.autonomousapps.dependency-analysis") version "2.18.0"
+        id("com.autonomousapps.dependency-analysis") version "2.19.0"
     }
 
     includeBuild("build-logic")

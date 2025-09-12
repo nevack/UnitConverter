1e7765de57e61577191efb24c259ca16add3a374
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-09-12 11:41:27 +0300

    Roll KSP 2.0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 
     plugins {
         val kotlinVersion = "2.2.20"
-        val kspSubVersion = "2.0.2"
+        val kspSubVersion = "2.0.3"
         id("com.github.ben-manes.versions") version "0.52.0"
         id("com.diffplug.spotless") version "7.2.1"
         id("com.android.application") version "8.13.0"

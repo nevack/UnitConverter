13b4ab1c7035d63783b84be45c4ce49dd757a819
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-10-10 12:00:31 +0300

    Roll androidx.room 2.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,10 +60,10 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     // Room
-    implementation("androidx.room:room-common:2.8.0")
-    implementation("androidx.room:room-runtime:2.8.0")
-    ksp("androidx.room:room-compiler:2.8.0")
-    implementation("androidx.sqlite:sqlite:2.6.0")
+    implementation("androidx.room:room-common:2.8.2")
+    implementation("androidx.room:room-runtime:2.8.2")
+    ksp("androidx.room:room-compiler:2.8.2")
+    implementation("androidx.sqlite:sqlite:2.6.1")
     // Okio
     implementation("com.squareup.okio:okio:3.16.0")
     // Moshi

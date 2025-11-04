e331244d321310f5a51949dabd82c01adc81d187
Dzmitry Neviadomski <nevack.d@gmail.com>
2025-11-04 16:30:23 +0300

    Roll Kotlin 2.2.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         id("com.github.ben-manes.versions") version "0.53.0"
         id("com.diffplug.spotless") version "8.0.0"
         id("com.android.application") version "8.13.0"
-        id("org.jetbrains.kotlin.android") version "2.2.20"
+        id("org.jetbrains.kotlin.android") version "2.2.21"
         id("com.google.devtools.ksp") version "2.3.0"
         id("com.google.dagger.hilt.android") version "2.57.2"
         id("org.gradle.android.cache-fix") version "3.0.2"

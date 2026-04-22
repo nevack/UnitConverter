e27a977dc983c103246fd2c91549b92c2c806374
Dzmitry Neviadomski <nevack.d@gmail.com>
2026-04-23 01:47:19 +0300

    Roll KSP 2.3.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         id("com.android.application") version "9.2.0"
         id("org.jetbrains.kotlin.android") version "2.3.20"
         id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"
-        id("com.google.devtools.ksp") version "2.3.6"
+        id("com.google.devtools.ksp") version "2.3.7"
         id("com.google.dagger.hilt.android") version "2.59.2"
         id("org.gradle.android.cache-fix") version "3.0.3"
         id("com.autonomousapps.dependency-analysis") version "3.9.0"

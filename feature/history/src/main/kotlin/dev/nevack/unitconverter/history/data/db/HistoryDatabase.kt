2149f0b78cb47e6d36729c32dc8f36fb5384499d
Dzmitry Neviadomski <nevack.d@gmail.com>
2026-04-25 17:59:04 +0300

    Enable Room schema export and commit generated schema for HistoryDatabase v2

--- feature/history/src/main/kotlin/dev/nevack/unitconverter/history/data/db/HistoryDatabase.kt
+++ feature/history/src/main/kotlin/dev/nevack/unitconverter/history/data/db/HistoryDatabase.kt
@@ -3,7 +3,7 @@ package dev.nevack.unitconverter.history.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [HistoryItem::class], version = 2, exportSchema = false)
+@Database(entities = [HistoryItem::class], version = 2, exportSchema = true)
 abstract class HistoryDatabase : RoomDatabase() {
     abstract fun dao(): HistoryDao
 }

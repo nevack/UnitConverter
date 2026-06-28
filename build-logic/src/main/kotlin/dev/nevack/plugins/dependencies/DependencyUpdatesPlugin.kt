f3f74730f474fe36ed73813ee5d2213208f0e224
Dzmitry Neviadomski <nevack.d@gmail.com>
2026-06-28 15:53:29 +0300

    Fix Gradle deprecation

--- build-logic/src/main/kotlin/dev/nevack/plugins/dependencies/DependencyUpdatesPlugin.kt
+++ build-logic/src/main/kotlin/dev/nevack/plugins/dependencies/DependencyUpdatesPlugin.kt
@@ -5,13 +5,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.specs.Spec
 import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.provideDelegate
 
 class DependencyUpdatesPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("com.github.ben-manes.versions")
 
-        val ignoreConstraints: String? by target
+        val ignoreConstraints = target.findProperty("ignoreConstraints") as? String?
         val ignoreConstraintsOption = IgnoreConstraintsOption.from(ignoreConstraints)
 
         val rejectStrategy = buildRejectStrategy(ignoreConstraintsOption)
